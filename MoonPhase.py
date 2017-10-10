9d5cdf9f8bcade244ab96aad70ecfd3ac2d467e5
nalderto <noahlouisalderton@gmail.com>
2017-10-10 01:33:13 -0400

    Basic Functionality Added

---
+++ MoonPhase.py
@@ -0,0 +1,16 @@
+import urllib.request, json
+
+#Find User Location
+locationURL = 'http://freegeoip.net/json'
+openLocationURL = urllib.request.urlopen(locationURL).read()
+formatLocationURL = json.loads(openLocationURL)
+lat = formatLocationURL['latitude']
+lon = formatLocationURL['longitude']
+
+#Moon Phase Finder
+moonURL = "http://api.usno.navy.mil/rstt/oneday?date=today&coords=" + str(lat) + "," + str(lon)
+openMoonURL = urllib.request.urlopen(moonURL).read()
+formatMoonURL = json.loads(openMoonURL)
+fracillum = formatMoonURL['fracillum']
+curphase = formatMoonURL['curphase']
+print(curphase)
