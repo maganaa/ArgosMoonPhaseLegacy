20272ab5f075fa8a1c794fa5c8da062f09b8cf49
nalderto <noahlouisalderton@gmail.com>
2017-10-15 16:27:07 -0400

    Inverts the emojis to make highlighted portion match illumination of moon

--- MoonPhase.4h.py
+++ MoonPhase.4h.py
@@ -15,6 +15,8 @@ lon = formatLocationURL['longitude']
 moonURL = "http://api.usno.navy.mil/rstt/oneday?date=today&coords=" + str(lat) + "," + str(lon)
 openMoonURL = urllib.request.urlopen(moonURL).read()
 formatMoonURL = json.loads(openMoonURL)
+
+#Exception catching as fracillum and curphase don't appear on JSON if closestphase is exact
 try:
     fracillum = formatMoonURL['fracillum']
     curphase = formatMoonURL['curphase']
@@ -27,30 +29,34 @@ except KeyError:
     elif curphase == "First Quarter" or curphase == "Last Quarter":
         fracillum = "50%"
 
-
+#The emojis are inverted to make the highlighted portion of the moon emoji match the illuminated portion of the moon on GNOME
 if curphase == "New Moon":
-    print(emoji.emojize(":new_moon:"))
-
-elif curphase == "Waxing Crescent":
-    print(emoji.emojize(":waxing_crescent_moon:"))
-
-elif curphase == "First Quarter":
-    print(emoji.emojize(":first_quarter_moon:"))
-
-elif curphase == "Waxing Gibbous":
-    print(emoji.emojize(":waxing_gibbous_moon:"))
-
-elif curphase == "Full Moon":
     print(emoji.emojize(":full_moon:"))
 
-elif curphase == "Waning Gibbous":
+elif curphase == "Waxing Crescent":
     print(emoji.emojize(":waning_gibbous_moon:"))
 
-elif curphase == "Last Quarter":
+elif curphase == "First Quarter":
     print(emoji.emojize(":last_quarter_moon:"))
 
-elif curphase == "Waning Crescent":
+elif curphase == "Waxing Gibbous":
     print(emoji.emojize(":waning_crescent_moon:"))
 
+elif curphase == "Full Moon":
+    print(emoji.emojize(":new_moon:"))
+
+elif curphase == "Waning Gibbous":
+    print(emoji.emojize(":waxing_crescent_moon:"))
+
+elif curphase == "Last Quarter":
+    print(emoji.emojize(":first_quarter_moon:"))
+
+elif curphase == "Waning Crescent":
+    print(emoji.emojize(":waxing_gibbous_moon:"))
+
+else:
+    print("Error")
+
 print("---")
+print(curphase)
 print(fracillum)
